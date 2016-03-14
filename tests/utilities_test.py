dd8f8fdd2eb1abe5a393283fa84270813312132a
AbdealiJK <abdealikothari@gmail.com>
2016-03-14 11:28:45 +0530

    file_metadata: Add utilities module

---
+++ tests/utilities_test.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+
+from file_metadata.utilities import make_temp
+
+
+class MakeTempTest(unittest.TestCase):
+
+    def test_make_temp_default(self):
+        with make_temp() as testfile:
+            testfilename = testfile
+            self.assertTrue(os.path.isfile(testfile))
+        self.assertFalse(os.path.exists(testfilename))
+
+    def test_make_temp_kwargs(self):
+        with make_temp(suffix=".orig", prefix="pre") as testfile:
+            testfilename = testfile
+            self.assertTrue(testfile.endswith(".orig"))
+            self.assertTrue(os.path.basename(testfile).startswith("pre"))
+        self.assertFalse(os.path.exists(testfilename))
