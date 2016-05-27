79c868aedb4c50a4bc088a1691c152df253ed7e2
AbdealiJK <abdealikothari@gmail.com>
2016-05-27 15:46:41 +0530

    _compat: Add function to parse old ffprobe output
    Parse output from the older versions of avprode/ffprobe. The -of or
    -print_format argument was added in versions 0.9+. This allows json
    output. But in older versions like 0.8.17 which is used in ubuntu
    precise, json output is not possible. In such cases, this function
    can be used to parse the output.

--- tests/_compat_test.py
+++ tests/_compat_test.py
@@ -6,7 +6,7 @@ from __future__ import (division, absolute_import, unicode_literals,
 import subprocess
 import sys
 
-from file_metadata._compat import check_output
+from file_metadata._compat import check_output, ffprobe_parser
 from tests import unittest
 
 
@@ -18,3 +18,43 @@ class CheckOutputTest(unittest.TestCase):
     def test_nonzero_exit(self):
         self.assertRaises(subprocess.CalledProcessError, check_output,
                           [sys.executable, '-c', 'unknown_func()'])
+
+
+class FFProbeParserTest(unittest.TestCase):
+    def test_streams(self):
+        data = ffprobe_parser("[STREAM]\n"
+                              "index=0\n"
+                              "[/STREAM]\n"
+                              "[STREAM]\n"
+                              "index=1\n"
+                              "[/STREAM]")
+        self.assertNotIn('format', data)
+        self.assertIn('streams', data)
+        self.assertEqual(len(data['streams']), 2)
+        self.assertEqual(data['streams'][0]['index'], '0')
+        self.assertEqual(data['streams'][1]['index'], '1')
+
+    def test_stream(self):
+        data = ffprobe_parser("[STREAM]\n"
+                              "index=0\n"
+                              "codec_time_base=0/1\n"
+                              "codec_tag=0x0000\n"
+                              "duration=10.833333\n"
+                              "[/STREAM]")
+        self.assertNotIn('format', data)
+        self.assertIn('streams', data)
+        self.assertEqual(len(data['streams']), 1)
+        self.assertEqual(data['streams'][0]['index'], '0')
+        self.assertEqual(data['streams'][0]['codec_tag'], '0x0000')
+        self.assertEqual(data['streams'][0]['codec_time_base'], '0/1')
+        self.assertEqual(data['streams'][0]['duration'], '10.833333')
+
+    def test_format(self):
+        data = ffprobe_parser("[FORMAT]\n"
+                              "filename=path/to/file.ext\n"
+                              "format_name=ogg\n"
+                              "[/FORMAT]\n")
+        self.assertNotIn('streams', data)
+        self.assertIn('format', data)
+        self.assertEqual(data['format']['filename'], 'path/to/file.ext')
+        self.assertEqual(data['format']['format_name'], 'ogg')
