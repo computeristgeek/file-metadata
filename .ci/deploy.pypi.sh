178e282fca983a3ffa90b8c404e9fe28af6054d9
AbdealiJK <abdealikothari@gmail.com>
2016-06-03 19:32:45 +0530

    deploy.pypi: Don't check pypi version
    The package installation is much more complicated now. Let's not
    check the version in rultor...

--- .ci/deploy.pypi.sh
+++ .ci/deploy.pypi.sh
@@ -11,13 +11,3 @@ echo "Uploading file-metadata to pypi"
 pip3 install twine wheel
 python3 setup.py sdist bdist_wheel
 twine upload dist/* -u "$PYPIUSER" -p "$PYPIPW"
-
-sudo apt-get -qq -y install python3-dev
-
-echo "Installing file-metadata from pypi"
-pip3 install --pre file-metadata==`cat file_metadata/VERSION` --upgrade
-pypi_version=`cd .. && python3 -c "import file_metadata; print(file_metadata.__version__)"`
-repo_version=`cat file_metadata/VERSION`
-
-echo versions: pip=$pypi_version repo=$repo_version
-[ $pypi_version = $repo_version ]
