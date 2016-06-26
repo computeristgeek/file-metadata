c85dd41e8616052f24db7837c3be8d67fc079ef0
AbdealiJK <abdealikothari@gmail.com>
2016-06-26 08:12:10 +0530

    setup: Revamp setup to handle deps better
    Have a class for every dependency which checks for
    the required packages correctly.

--- setup.py
+++ setup.py
@@ -5,72 +5,107 @@ from __future__ import (division, absolute_import, unicode_literals,
                         print_function)
 
 import os
-import platform
-import subprocess
 import sys
+from distutils import log
+from distutils.errors import DistutilsSetupError
 
-from setuptools import find_packages, setup
+import setupdeps
+
+log.set_verbosity(log.INFO)
+
+setup_deps = [
+    setupdeps.Distro(),
+    setupdeps.SetupTools()
+]
+
+install_deps = [
+    # Core deps
+    setupdeps.LibMagic(),
+    setupdeps.PythonMagic(),
+    setupdeps.Six(),
+    setupdeps.ExifTool(),
+    setupdeps.AppDirs(),
+    # Image deps
+    setupdeps.PathLib(),
+    setupdeps.LibLZMA(),
+    setupdeps.LZMA(),
+    setupdeps.Pillow(),
+    setupdeps.Numpy(),
+    setupdeps.Dlib(),
+    setupdeps.ScikitImage(),
+    setupdeps.MagickWand(),
+    setupdeps.Wand(),
+    setupdeps.LibZBar(),
+    setupdeps.ZBar(),
+    setupdeps.JavaJRE(),
+    setupdeps.PyColorName(),
+    # Audio video deps
+    setupdeps.FFProbe(),
+]
 
 
-with open(os.devnull, 'w') as nul:
-    # Check if exiftool or perl is installed.
-    try:
-        out = subprocess.check_call(['perl', '-v'], stdout=nul, stderr=nul)
-    except (OSError, subprocess.CalledProcessError):
+def check_deps(deplist):
+    failed_deps = []
+    for dep in deplist:
         try:
-            out = subprocess.check_call(['exiftool', '-ver'], stdout=nul,
-                                        stderr=nul)
-        except (OSError, subprocess.CalledProcessError):
-            print('`perl` (https://www.perl.org/) or `exiftool` '
-                  '(http://www.sno.phy.queensu.ca/~phil/exiftool/) '
-                  'need to be installed and made available in your PATH. '
-                  'On Ubuntu, you can install perl with '
-                  '`sudo apt-get install perl` or install exiftool with '
-                  '`sudo apt-get install exiftool`.')
-            sys.exit(1)
+            log_msg = dep.check()
+            log.info(dep.name + ' - ' + log_msg)
+        except setupdeps.CheckFailed as err:
+            log.info(dep.name + ' - ' + err.args[0])
+            failed_deps.append(dep)
 
-    # Check if java is installed.
-    try:
-        out = subprocess.check_call(['java', '-version'], stdout=nul,
-                                    stderr=nul)
-    except (OSError, subprocess.CalledProcessError):
-        print('`java` (https://java.com/) needs to be installed and needs to '
-              'be made available in your PATH. If using Ubuntu, you can do '
-              '`sudo apt-get install openjdk-7-jre`')
-        sys.exit(1)
+    if len(failed_deps) > 0:
+        msg = 'Some dependencies could not be installed automatically: '
+        msg += ", ".join(i.name for i in failed_deps)
+        for dep in failed_deps:
+            install_msg = dep.install_help_msg()
+            if install_msg:
+                msg += '\n* ' + install_msg
+        raise DistutilsSetupError(msg)
+    return deplist
 
-    # Check if avprobe of ffprobe is installed if system is not Linux.
-    # If it's linux, we use static builds from http://johnvansickle.com/libav/
-    if platform.system() != 'Linux':
-        try:
-            out = subprocess.check_call(['ffprobe', '-version'], stdout=nul,
-                                        stderr=nul)
-        except (OSError, subprocess.CalledProcessError):
-            try:
-                out = subprocess.check_output(['avprobe', '-version'],
-                                              stdout=nul, stderr=nul)
-            except (OSError, subprocess.CalledProcessError):
-                print('`ffprobe` (https://ffmpeg.org/ffprobe.html) or '
-                      '`avprobe` (http://libav.org/documentation/avprobe.html)'
-                      'need to be installed and made available in your PATH. '
-                      'If using Ubuntu, you can install avprobe with '
-                      '`sudo apt-get install libav-tools`.')
-            sys.exit(1)
+
+def get_install_requires(deplist):
+    packages = []
+    for dep in check_deps(deplist):
+        packages += dep.get_install_requires()
+    return packages
 
 
 def read_reqs(reqs_filename):
     reqs = open(reqs_filename).read().strip().splitlines()
     return list(i for i in reqs if not (i.startswith('#') or len(i) == 0))
 
-required = read_reqs('requirements.txt')
-test_required = read_reqs('test-requirements.txt')
-VERSION = open(os.path.join('file_metadata', 'VERSION')).read().strip()
 
-if sys.version_info >= (3,):
-    # mock is not required for python 3
-    test_required.remove('mock')
+if __name__ == '__main__':
+    log.info('Check and install dependencies required for setup:')
+    setup_packages = []
+    for dep in check_deps(setup_deps):
+        setup_packages += dep.get_setup_requires()
 
-if __name__ == "__main__":
+    if setup_packages:
+        log.info('Some required packages required for file-metadata\'s setup '
+                 'were not found: {0}.\nInstalling them with `pip` ...'
+                 .format(", ".join(setup_packages)))
+        out = setupdeps.setup_install(setup_packages)
+        if out is not True:
+            raise DistutilsSetupError(
+                'Unable to install package(s) required by the setup script '
+                'using pip: {0}\nReport this issue to the maintainer.\n'
+                'Temporarily, this could be solved by running: '
+                '`python -m pip install {1}`.'
+                .format(", ".join(setup_packages), " ".join(setup_packages)))
+
+    log.info('Check dependencies required for using file-metadata:')
+    install_required = get_install_requires(install_deps)
+    test_required = read_reqs('test-requirements.txt')
+    VERSION = open(os.path.join('file_metadata', 'VERSION')).read().strip()
+
+    if sys.version_info >= (3,):  # mock is not required for python 3
+        test_required.remove('mock')
+
+    from setuptools import find_packages, setup
+
     setup(name='file-metadata',
           version=VERSION,
           description='Helps to find structured metadata from a given file.',
@@ -78,13 +113,14 @@ if __name__ == "__main__":
           author_email="dr.trigon@surfeu.ch",
           maintainer="AbdealiJK",
           maintainer_email='abdealikothari@gmail.com',
+          license="MIT",
           url='https://github.com/AbdealiJK/file-metadata',
           packages=find_packages(exclude=["build.*", "tests.*", "tests"]),
-          install_requires=required,
+          # Dependency management by pip
+          install_requires=install_required,
           tests_require=test_required,
-          license="MIT",
           # Setuptools has a bug where they use isinstance(x, str) instead
-          # of basestring. Because of this we convert it to str.
+          # of basestring. Because of this we convert it to str for Py2.
           package_data={str('file_metadata'): [str("VERSION")]},
           # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
           classifiers=[
